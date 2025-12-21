9ed1ef6dcb9d25869332aece34c5bea3a2480d40
abed-andrea <andrea.abed03@gmail.com>
2025-12-21 15:04:43 -0800

    Define ContactMessage model and verify successful table createion via db_test

--- app/db_test.py
+++ app/db_test.py
@@ -1,13 +1,32 @@
-from sqlalchemy import text 
-from app.db import SessionLocal
+# FIRST TEST, handshake to see if connection between sqlalchemy and postgres db existed
+    # command to test: docker compose exec fast-api python -m app.db_test
+# from sqlalchemy import text 
+# from app.db import SessionLocal
+
+# def main():
+#     db = SessionLocal()
+#     try:
+#         result = db.execute(text("SELECT 1")).scalar()
+#         print("DB test result:", result)
+#     finally:
+#         db.close()
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+# test for initializing database (check is ContactMessage table is created properly)
+# docker compose exec fast-api python -m app.db_test
+# docker-compose exec db psql -U ${POSTGRES_USER} -d ${POSTGRES_DB}
+from app.db import engine, Base, SessionLocal
+from app.models import ContactMessage
+from sqlalchemy import text
 
 def main():
-    db = SessionLocal()
-    try:
-        result = db.execute(text("SELECT 1")).scalar()
-        print("DB test result:", result)
-    finally:
-        db.close()
+    print("Creating tables...")
+    Base.metadata.create_all(bind=engine)
+    print("Tables Created successfully!")
 
 if __name__ == "__main__":
     main()
---
+++ app/models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import String, Text, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db import Base
+
+
+# python class definition, inheriting from Base = telling sqlalchemy this is not a regular class, it is a blueprint for database table
+class ContactMessage(Base):
+    __tablename__ = "contact_messages" # name the table
+
+    # The Left Side (id: Mapped[int]): This is the Python Type. It is purely for your IDE and for FastAPI to understand the data format.
+    # The Right Side (= mapped_column(...)): This is the Database Configuration. It handles the actual SQL rules like length, primary keys, and constraints.
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(255))
+    message: Mapped[str] = mapped_column(Text())
+    create_at: Mapped[datetime] = mapped_column(server_default=func.now())
